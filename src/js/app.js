a7696c32bad830cb6b3a54ed389f0d6b907e5f20
David <davidmunteanonti@gmail.com>
2025-10-02 03:03:34 +0200

    usuario3 Añade funcion  para func: estilsCSS

--- src/js/app.js
+++ src/js/app.js
@@ -8,3 +8,8 @@ function changeHTMLAtribute() {
   document.getElementById("img").src =
     "https://imgs.search.brave.com/PBddIfJe7uqWTXY9dVikjolP0RiGn-quGaBDNxzi_BA/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly93NS5n/aWZmaXRzc3RhdGlj/LmNvbS9waWNzL2M1/MDQvMzY1Mjc2XzIu/anBn";
 }
+
+//funcion para cambiar el estilo css del p
+function changeCSSstyle() {
+  document.getElementById("texto").style.color = "red";
+}
